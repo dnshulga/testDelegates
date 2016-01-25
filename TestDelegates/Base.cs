b3fd688bfe7f3b1aac46ccfcd762c873984b3fe3
dnshulga <dnshulga@ya.ru>
2016-01-26 01:07:03 +0200

    first debug

---
+++ TestDelegates/Base.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDelegates
+{
+    public delegate void Print(string param);
+    class Base
+    {
+        public string Name { get; set; }
+
+        public void Output(string _name, Print method)
+        {
+            method(this.Name);
+        }
+    }
+}
---
+++ TestDelegates/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Base b1 = new Base();
+            b1.Name = "DimaShulga";
+            Print p = testOfDelegate;
+            b1.Output(b1.Name, p);
+        }
+
+        static void testOfDelegate(string arg)
+        {
+            Console.WriteLine(arg + " is using the Delegate method");
+        }
+    }
+}
